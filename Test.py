a4a9fc9c871354947218eb754d3c7c8b529aaee5
Javier Linzoain Pedraza <115028961+Linzpe@users.noreply.github.com>
2024-06-04 10:50:13 +0200

    Delete Test.py

--- Test.py
+++
@@ -1,7 +0,0 @@
-def create_test_file():
-    file_path = '/var/jenkins_home/Test.txt'
-    with open(file_path, 'w') as file:
-        file.write("Este es un test de funcionamiento")
-
-if __name__ == "__main__":
-    create_test_file()
